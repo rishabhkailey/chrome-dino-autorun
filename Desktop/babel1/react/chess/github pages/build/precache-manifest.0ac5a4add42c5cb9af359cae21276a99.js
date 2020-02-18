3745c45bd355d28665f02d6ee54f7d20973aeacb
rishabhkailey <rishabhveersinghkailay@gmail.com>
2020-02-18 18:49:43 +0530

    first-commit

---
+++ Desktop/babel1/react/chess/github pages/build/precache-manifest.0ac5a4add42c5cb9af359cae21276a99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdf3987e99e134fa32dd2b78a7e5b9f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5e7a2881127a83b6baa3",
+    "url": "/static/css/main.4d41f9a2.chunk.css"
+  },
+  {
+    "revision": "cb8681019a96526b8a7d",
+    "url": "/static/js/2.b1fda4e3.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.b1fda4e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e7a2881127a83b6baa3",
+    "url": "/static/js/main.68ce6ea9.chunk.js"
+  },
+  {
+    "revision": "493cf4213dc92ea26a05",
+    "url": "/static/js/runtime-main.335fa1a8.js"
+  }
+]);
